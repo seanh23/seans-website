849c838ee1d30ffdb567b933b9030615613d503d
seanh23 <seanharmond@gmail.com>
2024-06-29 17:41:35 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Sample data
+const learningItems = [
+    "JavaScript",
+    "HTML & CSS",
+    "React"
+];
+
+const books = [
+    "Clean Code by Robert C. Martin",
+    "The Pragmatic Programmer by Andrew Hunt and David Thomas"
+];
+
+// Function to add items to a list
+function populateList(items, listId) {
+    const list = document.getElementById(listId);
+    items.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.appendChild(li);
+    });
+}
+
+// Populate lists when the page loads
+window.onload = function() {
+    populateList(learningItems, 'learning-list');
+    populateList(books, 'book-list');
+};
+
+// You can add more functions here, such as adding new blog posts, learning items, or books
